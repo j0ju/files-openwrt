c2724e9297ec000ac4097c6515466dbc10919f31
Joerg Jungermann <jjungermann@1und1.de>
2015-01-21 14:29:29 +0100

    patch.sh: added vim folding

--- patch.sh
+++ patch.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
+# vim: foldmethod=marker:foldmarker=#{,#}
 BASEDIR="${0%/*}"
 set -e
 
-# WLAN - patch regulatory domain
+#{ WLAN - patch regulatory domain
 # to be compliant with the local freq limits use the config
 gcc -o "$BASEDIR/reghack" "$BASEDIR/reghack.c"
 find "$BASEDIR" -name "cfg80211.ko" -exec "$BASEDIR/reghack" {} \;
 find "$BASEDIR" -name "ath.ko" -exec "$BASEDIR/reghack" {} \;
 rm -f "$0" "$BASEDIR/reghack" "$BASEDIR/reghack.c"
-
-
-# modify preinit to load from /etc/modules-boot.d/ before
+#}
+#{ modify preinit to load from /etc/modules-boot.d/ before
 sbin=init
 mv "$BASEDIR/sbin/$sbin" "$BASEDIR/lib/$sbin"
 cat > "$BASEDIR/sbin/$sbin" << EOF
@@ -22,23 +22,19 @@ umount /proc
 exec /lib/$sbin
 EOF
 chmod 755 "$BASEDIR/sbin/$sbin"
-
-
-# remove some files
+#}
+#{ remove some files in /etc/init.d and /etc/rc.d
 rm -f \
   "$BASEDIR"/etc/init.d/esi \
   "$BASEDIR"/etc/rc.d/[SK]??esi \
   "$BASEDIR"/etc/rc.d/[SK]??telnet \
   "$BASEDIR"/etc/rc.d/[SK]??pf \
   "$BASEDIR"/etc/rc.d/[SK]??pf6 \
-#
-
-
-# create SuSe like rc$SCRIPT links to init scripts
+#}
+#{ create SuSe like rc$SCRIPT links to init scripts
 __PREFIX__="$BASEDIR" "$BASEDIR/usr/local/sbin/update-rc"
-
-
-# ignore block sizes try to squeeze more out of some file larger than
+#}
+#{ ignore block sizes try to squeeze more out of some file larger than
 MAX_SIZE=150K
 PACKER="disabled:disabled" # default, disabled
 #PACKER="bzip2:bz2.exe"
@@ -68,4 +64,4 @@ find \
 suffix="*"
 eval "preamble=\"$PREAMBLE\""
 rm -f "$BASEDIR"/$preamble
-
+#}
