6df566172ee37fe677b3b3f5db8060ea238c8e01
jjungermann <jjungermann@1und1.de>
2015-10-16 17:29:44 +0200

    initial for openwrt 15.05 dockstar

--- bin/login.sh
+++ bin/login.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
+# default umask and PATH
+umask 0022
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+# unexport all other variables
+for env in $(env | cut -f1 -d= | grep ^[A-Za-z_]) ""; do
+  [ -z "$env" ] && continue
+  case "$env" in
+    TERM | PATH ) continue ;;
+  esac
+  export "$env"=
+  unset $env
+done
+
+# export HOME
 UID="$(awk '/^Uid:/ { print $2}' /proc/$$/status)"
 export HOME="$(awk -F: '/^[-a-zA-Z0-9]+:[^:]*:'"$UID"':/ {print $6}' /etc/passwd)"
 cd "$HOME"
 
 exec /bin/ash --login
+
--- root/scripts/find-btrfs-files-with-errors.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-dmesg | sed -n 's/^.*btrfs.*ino\(de\)\? \([0-9]\+\).*$/\2/ p' | sort -u | xargs -n 1 find / -xdev -inum
