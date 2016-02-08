896f0157d8bd1a84be3a183b4d49305558824f8d
jjungermann <jj@0x2f.org>
2016-02-08 13:22:14 +0100

    patch.sh: change netifd/proto calls to full pathnames

--- patch.sh
+++ patch.sh
@@ -20,3 +20,31 @@ chmod 755 "$BASEDIR/sbin/$sbin"
 __PREFIX__="$BASEDIR" "$BASEDIR/usr/local/bin/update-rc"
 #}
 
+find_bin() {
+  [ -z "$1" ] && return
+  local BIN_DIRS="/bin /sbin /usr/sbin /usr/bin"
+  local dir bin
+  for dir in $BIN_DIRS; do
+     bin="$BASEDIR/$dir/$1"
+     [ -x "$bin" ] && break || :
+  done
+  [ -x "$bin" ] && echo "$dir/$1" || :
+}
+
+grep -E -o '(proto_run_command [^ ]+ )([^\ ]+)' "$BASEDIR"/lib/netifd/proto/* 2> /dev/null | while read file; do
+  cd "$BASEDIR"
+  file="${file#$BASEDIR}"
+  line="${file#*:}"
+  file="${file%%:*}"
+  prog="${line##* }"
+  case "${prog}" in
+    /* ) continue ;;
+  esac
+
+  real_proc="$(find_bin "$prog")"
+  [ -n "$real_proc" ] || continue
+  [ -x "$BASEDIR/$real_proc" ] || continue
+
+  sed -i -re 's@(proto_run_command [^ ]+ )([^ ]+)@\1'"$real_proc"'@' "$BASEDIR/$file"
+done
+
