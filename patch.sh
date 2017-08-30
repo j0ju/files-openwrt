b2a78d8ee77f3e0e66a81b561b1025d4e9f844b8
Joerg Jungermann <joerg.jungermann@fhe3.com>
2017-08-30 19:14:23 +0200

    * move to leded 17.01

--- patch.sh
+++ patch.sh
@@ -2,6 +2,7 @@
 # vim: foldmethod=marker:foldmarker=#{,#}
 BASEDIR="${0%/*}"
 set -e
+set -x
 
 #{ modify preinit to load from /etc/modules-boot.d/ before
 sbin=init
@@ -16,14 +17,63 @@ exec /lib/$sbin
 EOF
 chmod 755 "$BASEDIR/sbin/$sbin"
 #}
-#{ remove some files in /etc/init.d and /etc/rc.d
-rm -f \
-  "$BASEDIR"/etc/init.d/esi \
-  "$BASEDIR"/etc/rc.d/[SK]??esi \
-  "$BASEDIR"/etc/rc.d/[SK]??telnet \
-  "$BASEDIR"/etc/rc.d/[SK]??pf \
-  "$BASEDIR"/etc/rc.d/[SK]??pf6 \
-#}
 #{ create SuSe like rc$SCRIPT links to init scripts
 __PREFIX__="$BASEDIR" "$BASEDIR/usr/local/bin/update-rc"
 #}
+#{ disable services
+  RC_TO_DISABLE=
+  RC_TO_DISABLE="$RC_TO_DISABLE qos"
+  RC_TO_DISABLE="$RC_TO_DISABLE firewall"
+  RC_TO_DISABLE="$RC_TO_DISABLE siproxd"
+  RC_TO_DISABLE="$RC_TO_DISABLE openvpn"
+  RC_TO_DISABLE="$RC_TO_DISABLE fastd"
+  RC_TO_DISABLE="$RC_TO_DISABLE telnet"
+  RC_TO_DISABLE="$RC_TO_DISABLE qos"
+  RC_TO_DISABLE="$RC_TO_DISABLE firewall"
+  RC_TO_DISABLE="$RC_TO_DISABLE siproxd"
+  RC_TO_DISABLE="$RC_TO_DISABLE openvpn"
+  RC_TO_DISABLE="$RC_TO_DISABLE fastd"
+  RC_TO_DISABLE="$RC_TO_DISABLE telnet"
+  RC_TO_DISABLE="$RC_TO_DISABLE nfsd"
+  RC_TO_DISABLE="$RC_TO_DISABLE portmap"
+  RC_TO_DISABLE="$RC_TO_DISABLE btrfs-scan"
+  RC_TO_DISABLE="$RC_TO_DISABLE usbmode"
+  RC_TO_DISABLE="$RC_TO_DISABLE ipset-dns"
+  RC_TO_DISABLE="$RC_TO_DISABLE keepalived"
+  RC_TO_DISABLE="$RC_TO_DISABLE bird4 bird6"
+
+  for _rc in $RC_TO_DISABLE; do
+    rm -f "$BASEDIR/etc/rc.d/"S[0-9][0-9]$_rc
+  done
+
+#}
+#{ ensure full pathname for netifd protos
+find_bin() {
+  [ -z "$1" ] && return
+  local BIN_DIRS="/bin /sbin /usr/sbin /usr/bin"
+  local dir bin
+  for dir in $BIN_DIRS; do
+     bin="$BASEDIR/$dir/$1"
+     [ -x "$bin" ] && break || :
+  done
+  [ -x "$bin" ] && echo "$dir/$1" || :
+}
+
+grep -E -o '(proto_run_command [^ ]+ )([^\ ]+)' "$BASEDIR"/lib/netifd/proto/* 2> /dev/null | while read file; do
+  cd "$BASEDIR"
+  file="${file#$BASEDIR}"
+  line="${file#*:}"
+  file="${file%%:*}"
+  prog="${line##* }"
+  case "${prog}" in
+    /* ) continue ;;
+  esac
+
+  real_proc="$(find_bin "$prog")"
+  [ -n "$real_proc" ] || continue
+  [ -x "$BASEDIR/$real_proc" ] || continue
+
+  sed -i -re 's@(proto_run_command [^ ]+ )([^ ]+)@\1'"$real_proc"'@' "$BASEDIR/$file"
+done
+#}
+
